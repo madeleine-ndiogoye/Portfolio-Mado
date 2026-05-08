706a56167031602870040d589df1f03ec2a671f6
madeleine-ndiogoye <ndiogoyemadeleineraissa@gmail.com>
2026-05-08 11:38:01 +0000

    Add files via upload

---
+++ appli.py
@@ -0,0 +1,56 @@
+import streamlit as st
+
+# configuration de la page
+st.set_page_config(page_title="CV - Madeleine Ndiogoye", layout="wide")
+
+with st.sidebar:
+    st.image("image.jpeg", width=190)
+    st.write("_____________________________")
+    st.write("📍Addresse: Dakar,Sénégal")
+    st.write("📧Email: ndiogoyemadeleineraissa@gmail.com")
+    st.write("je suis disponible pour toute possibilité d'entretien")
+    st.write("_____________________________")
+
+st.title("Madeleine Raïssa Ndiogoye")
+st.header("Technicienne supérieure en Sciences Agricoles, Alimentaires et Nutritionnelles")
+st.write("_______________________________")
+
+# ------- PROFIL -----------
+st.header("Profil")
+st.write("Technicienne supérieure en Sciences Agricoles, Alimentaires et Nutritionnelles, je suis passionnée par l'agriculture et ces enjeux. Je souhaite appronfondir mes connaissances et acquérir davantage des expériences pratiques. Mon pbjectif est de contribuer aux projets qui favorisent une agriculture durable et une sécurité alimentaire meilleure.")
+st.write("_______________________________")
+
+# ----- COMPETENCES --------
+st.header("Compétences")
+st.write("* Maitrise des techniques de gestion des cultures")
+st.write("* Connaissances sur la lutte et la préservation contre les ravageurs des cultures")
+st.write("* Analyse des sols")
+st.write("* Irragation et gestion de l'eau")
+st.write("* Pack office")
+st.write("* Canva")   
+st.write("_________________")
+
+# ------- EXPERIENCE -------------
+st.header("Expériences")
+st.write("* Semptembre 2025: Stage à la Direction Régionale de Développement Rural (DRDR): Participation aux activités agricoles")
+st.write("* 2015 et 2017: Exploitation familiale: travail sur les terres agricoles (culturess du mil et d'arachide)")
+st.write("_________________")
+
+# ------ FORMATION --------
+st.header("Formation")
+st.write("-Licence en Sciences Agricoles, Alimentaires et Nutritionnelles -- Université Amadou Mahtar Mbow -- Diamniadio")
+st.write("-Diplôme de  Baccalauréat Scientifique")
+
+
+
+    
+    
+
+    
+
+    
+    
+
+
+
+
